897db7b86b290320d6f37fbc034c562c82a937e6
ivisnauskis <invisnauskis@gmail.com>
2020-10-14 11:14:11 +0300

    port

--- index.js
+++ index.js
@@ -117,8 +117,7 @@ app.post("/api/persons", (request, response) => {
   response.json(person);
 });
 
-const PORT = 3001;
-app.listen(PORT);
-console.log(
-  `Server running on port ${PORT} / http://localhost:3001/api/persons`
-);
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
