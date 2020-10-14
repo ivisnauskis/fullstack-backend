9c81b51d72248dfdb01fa56ce8bf499b3174d896
ivisnauskis <invisnauskis@gmail.com>
2020-10-14 11:25:51 +0300

    build

---
+++ build/precache-manifest.756895749564ba31fe5140e6fa462f5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e226bf18736d9233142d742b4509b050",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80e0de213eb31d234ca0",
+    "url": "/static/css/main.a044d623.chunk.css"
+  },
+  {
+    "revision": "5e4fbd03f064cf27d29c",
+    "url": "/static/js/2.1237afd9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1237afd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80e0de213eb31d234ca0",
+    "url": "/static/js/main.961eb0b0.chunk.js"
+  },
+  {
+    "revision": "c5e8f5ba0b491ea9966c",
+    "url": "/static/js/runtime-main.222d136d.js"
+  }
+]);
--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.use(
   })
 );
 app.use(cors());
+app.use(express.static("build"));
 
 let persons = [
   {
